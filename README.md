acc34de45da8dc71e3421470ddbc2d9bcfe1d7ef
19IT112 <19it112@charusat.edu.in>
2021-11-10 14:54:46 +0530

    second

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# event
+# eventmanagment
 
 A new Flutter project.
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,76 @@
-import 'package:event/auth.dart';
-import 'package:event/auth_screen.dart';
-import 'package:event/home_page.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:eventmanagment/Screens/Event_Screen.dart';
+import 'package:eventmanagment/Screens/auth_screen.dart';
+import 'package:eventmanagment/Screens/event_details_screen.dart';
+import 'package:eventmanagment/Screens/upload_pdf_screen.dart';
+import 'package:eventmanagment/models/auth.dart';
+import 'package:eventmanagment/models/uploadPDf.dart';
+import 'package:eventmanagment/models/userInfo.dart';
+import 'package:eventmanagment/widgets/event_add_page.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:provider/provider.dart';
+import 'models/event.dart';
 
-void main(List<String> args) async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(Myapp());
+  runApp(MyApp());
 }
 
-class Myapp extends StatelessWidget {
-  // const Myapp({Key? key}) : super(key: key);
-  final FirebaseAuth auth = FirebaseAuth.instance;
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
 
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    // Future.delayed(Duration(seconds: 3), () {
-    //   if (auth.currentUser == null) {
-    //     Navigator.of(context).pushReplacement(
-    //         MaterialPageRoute(builder: (context) => AuthScreen()));
-    //   } else {
-    //     Navigator.of(context).pushReplacement(
-    //         MaterialPageRoute(builder: (context) => HomePage()));
-    //   }
-    // });
     return MultiProvider(
       providers: [
-        Provider<Auth>(create: (context) => Auth(FirebaseAuth.instance)),
-        StreamProvider(
-          create: (context) => context.read<Auth>().authstateChanges,
-          initialData: null,
+        ChangeNotifierProvider<Events>(
+          create: (context) => Events(),
+        ),
+        ChangeNotifierProvider<Auth>(
+          create: (context) => Auth(),
+        ),
+        ChangeNotifierProvider<UploadPdf>(
+          create: (context) => UploadPdf(),
+        ),
+        ChangeNotifierProvider<UserInfo>(
+          create: (context) => UserInfo(),
         ),
       ],
-      child: MaterialApp(
-        title: "Event",
-        home: AuthWrapper(),
+      child: Consumer<Auth>(
+        builder: (context, auth, _) {
+          return MaterialApp(
+            home: User(),
+            routes: {
+              UploadPdfScreen.routeName: (ctx) => UploadPdfScreen(),
+              AddEventPage.routeName: (ctx) => AddEventPage(),
+              EventDetailScreen.routeName: (ctx) => EventDetailScreen(),
+              // EventScreen.routeName: (ctx) => EventScreen(),
+            },
+          );
+        },
       ),
     );
   }
 }
 
-class AuthWrapper extends StatelessWidget {
-  const AuthWrapper({Key? key}) : super(key: key);
-
+class User extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final user = context.watch<User?>();
-    // ignore: unnecessary_null_comparison
-    if (user != null) {
-      return HomePage();
-    } else {
-      return AuthScreen();
-    }
+    return StreamBuilder(
+      stream: Provider.of<Auth>(context).authstateChanges,
+      builder: (context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          final bool isloggedin = snapshot.hasData;
+          return isloggedin ? EventScreen() : AuthScreen();
+        }
+        return CircularProgressIndicator();
+      },
+    );
   }
 }
